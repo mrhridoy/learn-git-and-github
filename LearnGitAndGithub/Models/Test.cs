590c0dd2500d45c1fa98473976819c934e38a2f0
Mahinur Rahaman Hridoy <mrhridoy.net@outlook.com>
2025-12-20 00:02:41 +0600

    Test modifed lines added

--- LearnGitAndGithub/Models/Test.cs
+++ LearnGitAndGithub/Models/Test.cs
@@ -7,5 +7,10 @@
         public string Description { get; set; }
 
         public string Information { get; set; } 
+
+
+        public string Line2 { get; set; }   
+        public string Line3 { get; set; }
+        public string Line4 { get; set; }   
     }
 }
